d6b984450c8c75425debf5b6525b23a3bd72fb5d
nulldash <nulldash@yandex.ru>
2017-02-05 15:49:33 +0800

    Fixed some derp

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -160,7 +160,7 @@ DECLARE_PER_CPU(unsigned long, process_counts);
 extern int nr_processes(void);
 extern unsigned long nr_running(void);
 extern unsigned long nr_iowait(void);
-#if defined(CONFIG_INTELLI_PLUG)
+#ifdef CONFIG_INTELLI_PLUG
 extern unsigned long avg_nr_running(void);
 extern unsigned long avg_cpu_nr_running(unsigned int cpu);
 #endif
